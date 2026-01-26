63ba2cb80432d858b063062bc4b26abec423d8c8
Hari Om Purohit <hariompurohit910@gmail.com>
2026-01-26 14:01:32 +0530

    Complete linked list refactor with clean API and delete-by-position

--- 01-linked-list-core/linked_list_core.c
+++ 01-linked-list-core/linked_list_core.c
@@ -1,11 +1,38 @@
 /*
-TODO:
-1. Handle deletion of first node in delete_node()
-2. Add boundary checks in insert_at_position()
-3. Add list size tracking
-4. Improve loop_detection NULL checks
+PROJECT TODO (Next Steps):
+
+1. Split linked list into header (.h) and source (.c) files
+   - Move struct definitions and function prototypes to linked_list.h
+   - Move implementations to linked_list.c
+   - Keep main() as a client only
+
+2. Make ll_has_cycle() usable
+   - Print meaningful output in main() based on return value
+   - Optionally add a debug function to intentionally create a loop for testing
+
+3. Add return-value handling in main()
+   - Check success/failure for insert/delete operations
+   - Print user-friendly messages
+
+4. Add cleanup function
+   - Implement ll_clear() to free all nodes before exit
+
+5. Improve README
+   - Explain API design
+   - Document time complexity of operations
+   - Explain design decisions (no globals, size invariant)
 */
 
+
+
+/*
+PROJECT DONE:
+1. Track list size inside LinkedList (no external counters)
+2. Remove duplicate node creation logic
+3. Refactor to avoid global list head
+4. Implement delete-by-value and delete-by-position
+5. Improve loop_detection safety checks (Floyd algorithm)
+*/
 
 
 
@@ -13,42 +40,65 @@ TODO:
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//global struct node
 struct node
 {
     int data;
     struct node *next;
 };
-struct node *start = NULL;
+
+
+//global struct linked list
+struct LinkedList
+{
+    struct node *head;
+    int size;
+};
+
+
 
 //function prototypes
 
-void create_node(int val);
-void insert_at_start(int val);
-void insert_at_end(int val);
-void insert_at_position(int pos, int val);
-void reverse_list();
-void loop_detection();
-void display_list();
-void delete_node(int val);
+int ll_push_back(struct LinkedList *list, int val);
+int ll_push_front(struct LinkedList *list, int val);
+int ll_insert_at(struct LinkedList *list, int val, int pos);
+void ll_reverse(struct LinkedList *list);
+int ll_has_cycle(struct LinkedList *list);
+void ll_print(struct LinkedList *list);
+int ll_delete_value(struct LinkedList *list, int val);
+int ll_delete_at(struct LinkedList *list, int pos);
 
 
 int main() 
 {
-    int choice,val,no_of_nodes,position,size;
+    //local list of type struct LinkedList
+    struct LinkedList list = {NULL, 0};
+
+
+    int choice,val,no_of_nodes,position;
+    
     while (1)
     {
-        printf("\n1. create list");
+        printf("\n1. insert elements");
         printf("\n2. insert node at start");
-        printf("\n3. insert node at end");
-        printf("\n4. insert node at pos");
-        printf("\n5. reverse list");
-        printf("\n6. loop detection");
-        printf("\n7. display list");
-        printf("\n8. delete node");
+        printf("\n3. insert node at pos");
+        printf("\n4. reverse list");
+        printf("\n5. loop detection");
+        printf("\n6. display list");
+        printf("\n7. delete node");
+        printf("\n8. delete at position");
         printf("\n9. quit");
         printf("\nEnter your choice: ");
 
-        scanf("%d",&choice);
+        if (scanf("%d", &choice) != 1)//removing infi loop problem of scanf for wrong data type
+        {
+            printf("Invalid input. Enter a number.\n");
+
+            // clear input buffer
+            while (getchar() != '\n');
+                continue;
+        }
 
         switch (choice)
         {
@@ -59,52 +109,52 @@ int main()
             {
                 printf("\nenter the value: ");
                 scanf("%d",&val);
-                create_node(val);
+                ll_push_back(&list,val);
             }
             
             break;
         case 2:
             printf("\nenter the  element to insert at start: ");
             scanf("%d",&val);
-            insert_at_start(val);
+            ll_push_front(&list,val);
             break;
 
         case 3:
-            printf("\nenter the element to insert at end: ");
-            scanf("%d",&val);
-            insert_at_end(val);
-            break;
-
-        case 4:
             printf("\nenter the position: ");
             scanf("%d",&position);
             printf("\nenter the element: ");
             scanf("%d",&val);
-            insert_at_position(position,val);
+            ll_insert_at(&list,val,position);
+            break;
+        
+        case 4:
+            printf("\n");
+            ll_print(&list);
+            printf("\n reversing list.........");
+            ll_reverse(&list);
+            ll_print(&list);
             break;
         
         case 5:
-            printf("\n");
-            display_list();
-            printf("\n reversing list.........");
-            reverse_list();
-            display_list();
-            break;
-        
-        case 6:
             printf("\n Analyzing.........");
-            loop_detection();
+            ll_has_cycle(&list);
+            break;
+
+        case 6:
+            printf("\n list: ");
+            ll_print(&list);
             break;
 
         case 7:
-            printf("\n list: ");
-            display_list();
+            printf("\nenter the value to delete: ");
+            scanf("%d",&val);
+            ll_delete_value(&list,val);
             break;
 
         case 8:
-            printf("\nenter the value to delete: ");
-            scanf("%d",&val);
-            delete_node(val);
+            printf("\nenter the position to delete: ");
+            scanf("%d",&position);
+            ll_delete_at(&list,position);
             break;
 
         case 9:
@@ -122,64 +172,77 @@ int main()
 }
 
 
-void create_node(int val)
+int ll_push_back(struct LinkedList *list, int val)
 {
     struct node *tmp, *q;
     tmp = (struct node *)malloc(sizeof(struct node));
 
+    // if malloc is not able to provide memory
+    if(tmp == NULL)
+        return -1;
+
     (*tmp).data = val;
     tmp->next = NULL;
     
-    if(start == NULL)
-        start = tmp;
+
+    //case1: list is empty
+    if(list->head == NULL)
+    {
+        list->head = tmp;
+        list->size++;
+        return 0;
+    }
+
+    //case2: list is not empty
     else
     {
-        q = start;
+        q = list->head;
         while (q->next != NULL)
         {
             q=q->next;
         }
         q->next = tmp;
+        list->size++;
+        return 0;
     }
     
 }
 
-void insert_at_start(int val)
+int ll_push_front(struct LinkedList *list, int val)
 {
     struct node *tmp;
     tmp = (struct node *)malloc(sizeof(struct node));
+    if(tmp == NULL)
+        return -1;
     tmp->data = val;
-    tmp->next = start;
-    start = tmp;
+    tmp->next = list->head;
+    list->head = tmp;
+    list->size++;
+    return 0;
+}
+
+
+int ll_insert_at(struct LinkedList *list, int val,int pos)
+{
+    if (pos > (list->size)+1 || pos <= 0)
+    {
+        //printf("\nplease give appropiate size: ");
+        return -1;
+    }
+
+    //case1: at front
+    if (pos == 1)
+    {
+        ll_push_front(list, val);
+        return 0;
+    }
     
-}
-
-// unecessary doing same work as create node 
-void insert_at_end(int val)
-{
-    if(start == NULL)
-    {
-        create_node(val);
-        return;
-    }
+    //case2: at pos other than front
     struct node *tmp, *q;
     tmp = (struct node *)malloc(sizeof(struct node));
-    tmp->data = val;
-    tmp->next = NULL;
-    q=start;
-    while(q->next != NULL)
-    {
-        q=q->next;
-    }
-    q->next = tmp; 
-       
-}
-
-void insert_at_position(int pos, int val)
-{
-    struct node *tmp, *q;
-    tmp = (struct node *)malloc(sizeof(struct node));
-    q=start;
+    if(tmp == NULL)
+        return -1;
+    q=list->head;
     for (int i = 0; i < pos-2; i++)
     {
         q=q->next;
@@ -187,16 +250,17 @@ void insert_at_position(int pos, int val)
     tmp->next = q->next;
     tmp->data = val;
     q->next = tmp;  
-     
+    list->size++;
+    return 0; 
 }
 
-void reverse_list()
+void ll_reverse(struct LinkedList *list)
 {
-    if(start == NULL)//checking if list is empty
+    if(list->head == NULL)//checking if list is empty
         return;
     struct node  *curr, *prev, *nex;
     prev = NULL; //storing previous address
-    curr = start; // having current adress
+    curr = list->head; // having current adress
     while (curr->next != NULL)
     {
         nex = curr->next; //nex has adress of next node
@@ -204,36 +268,39 @@ void reverse_list()
         prev = curr; //updating previous as current one
         curr = nex; //since current link has been changed so going to a next link using nex
     }
+    //at last updating head
     curr->next = prev;
-    start = curr;
+    list->head = curr;
          
 }
 
 /// @brief checking if loop is present in list (using floyd algo)
-void loop_detection()
+int ll_has_cycle(struct LinkedList *list)
 { 
-    if(start == NULL)
-        return;
+    if(list->head == NULL)
+        return 0;
     struct node *slow, *fast;
-    slow = start;
-    fast = start;
-    while (fast->next != NULL && fast != NULL)
+    slow = list->head;
+    fast = list->head;
+
+    while (fast != NULL && fast->next != NULL)
     {
         slow = slow->next;
         fast = fast->next->next;
         if (slow == fast)
         {
-            printf("cycle exists");
-            return;
+            //printf("cycle exists");
+            return -1;
         }
     }
-    printf("no cycle exits");
+    //printf("no cycle exits");
+    return 0;
 }
 
-void display_list()
+void ll_print(struct LinkedList *list)
 {
     struct node *q;
-    q=start;
+    q=list->head;
     printf("\n");
     while (q != NULL)
     {
@@ -242,33 +309,74 @@ void display_list()
     }    
 }
 
-void delete_node(int val)
+int ll_delete_value(struct LinkedList *list, int val)
 {
-    if(start == NULL)
-        return;
-    struct node *tmp, *q;
-    q=start;
+    if (list->head == NULL)
+        return -1;
 
-    //if first element needs to be deleted
+    struct node *q = list->head;
+    struct node *tmp;
+
+    // Case 1: delete head
     if (q->data == val)
     {
-        tmp = start;
-        start = q->next;
-        free(tmp);
-        return;
+        list->head = q->next;
+        free(q);
+        list->size--;
+        return 0;
     }
-    
+
+    // Find node BEFORE the one to delete
     while (q->next != NULL && q->next->data != val)
     {
-        if (q->next == NULL)
-        {
-            printf("element not found !");
-            return;
-        }
-        
-        q=q->next;
+        q = q->next;
     }
+
+    // Value not found
+    if (q->next == NULL)
+        return -1;
+
+    // Delete the node
     tmp = q->next;
-    q->next = q->next->next;
+    q->next = tmp->next;
     free(tmp);
+    list->size--;
+
+    return 0;
+}
+
+int ll_delete_at(struct LinkedList *list, int pos)
+{
+    if (list == NULL || list->head == NULL)
+        return -1;
+
+    if (pos <= 0 || pos > list->size)
+        return -1;
+
+    struct node *tmp;
+
+    // Case 1: delete head
+    if (pos == 1)
+    {
+        tmp = list->head;
+        list->head = tmp->next;
+        free(tmp);
+        list->size--;
+        return 0;
+    }
+
+    struct node *q = list->head;
+
+    // move to node just before the one to delete
+    for(int i = 1; i < pos - 1; i++)
+    {
+        q = q->next;
+    }
+
+    tmp = q->next;
+    q->next = tmp->next;
+    free(tmp);
+    list->size--;
+
+    return 0;
 }
