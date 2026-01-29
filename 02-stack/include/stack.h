73a6831105424184a65be163ad467e8bcd6d6eab
Hari Om Purohit <hariompurohit910@gmail.com>
2026-01-29 21:02:00 +0530

    skeleton

---
+++ 02-stack/include/stack.h
@@ -0,0 +1,22 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include<stddef.h>
+
+typedef struct Stack Stack;
+
+/*init && clr (lifecycles)*/
+
+void stack_init(Stack *s);
+void stack_clear(Stack *s);
+
+/* core opr */
+int   stack_push(Stack *s, void *data);
+void *stack_pop(Stack *s);
+void *stack_peek(Stack );
+
+/*helper func*/
+int stack_is_empty(const Stack *s);
+int stack_size(const Stack *s);
+
+#endif
